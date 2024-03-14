7427d8896204c7a88d7253552902b49caa8a81dd
Pouria1100 <94846257+Pouria1100@users.noreply.github.com>
2024-03-14 13:07:20 +0000

    register service worker

--- src/main.js
+++ src/main.js
@@ -6,3 +6,17 @@ import './assets/main.css'
 new Vue({
   render: (h) => h(App)
 }).$mount('#app')
+
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
